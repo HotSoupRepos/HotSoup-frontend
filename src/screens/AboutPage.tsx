0ed831a3f46a797bcc4a323b7bdca0870197d085
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-07-20 19:38:02 -0500

    created new stylesheet and added styles to text and header on page

--- src/screens/AboutPage.tsx
+++ src/screens/AboutPage.tsx
@@ -4,12 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function AboutPage() {
   return (
     <View style={styles.container}>
-      <Text>
+      <Text style={styles.header}> About HotSoup</Text>
+      <Text style={styles.text}>
         HotSoup is an OpenSource project started by Robin Weitzman. It 
         is an entirely volunteer-based, co-op project to build a free
-        app those in need of a free meal.
-      </Text>
-      <Text>
+        app those in need of a free meal. {"\n"}
         This current version is the first iteration of this project so there may be some errors. If you'd like 
         to know more about this project or report any errors, contact robin@hotsoup.com.
         We appreciate your patience and assistance as we build further versions of this app!
@@ -19,12 +18,33 @@ export default function AboutPage() {
   );
 }
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fcb36a',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     padding: 100,
+//   },
+// });
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fcb36a',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 100,
+    padding: 24,
+    alignItems: "center"
   },
+  text: {
+    color: "#000",
+    fontSize: 14,
+    fontWeight: "bold"
+  },
+  header: {
+    color: "#61dafb",
+    fontSize: 30,
+    marginBottom: 36
+  }
 });
+
+
+
