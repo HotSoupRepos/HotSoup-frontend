e1b1f4c6ea122e936366da7c66162c3fc241b629
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-07-20 19:19:10 -0500

    added text and background color to page

--- src/screens/AboutPage.tsx
+++ src/screens/AboutPage.tsx
@@ -4,7 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function AboutPage() {
   return (
     <View style={styles.container}>
-      <Text>This is the about page!</Text>
+      <Text>
+        HotSoup is an OpenSource project started by Robin Weitzman. It 
+        is an entirely volunteer-based, co-op project to build a free
+        app those in need of a free meal.
+      </Text>
+      <Text>
+        This current version is the first iteration of this project so there may be some errors. If you'd like 
+        to know more about this project or report any errors, contact robin@hotsoup.com.
+        We appreciate your patience and assistance as we build further versions of this app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,9 @@ export default function AboutPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fcb36a',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 100,
   },
 });
