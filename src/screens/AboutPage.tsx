06dfba2eb807531b7eadc0268d80567d6121c379
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-07-20 19:59:14 -0500

    refined styles to match figma prototype

--- src/screens/AboutPage.tsx
+++ src/screens/AboutPage.tsx
@@ -4,16 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function AboutPage() {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}> About HotSoup</Text>
-      <Text style={styles.text}>
-        HotSoup is an OpenSource project started by Robin Weitzman. It 
-        is an entirely volunteer-based, co-op project to build a free
-        app those in need of a free meal. {"\n"}
-        This current version is the first iteration of this project so there may be some errors. If you'd like 
-        to know more about this project or report any errors, contact robin@hotsoup.com.
-        We appreciate your patience and assistance as we build further versions of this app!
-      </Text>
-      <StatusBar style="auto" />
+      <View style={styles.textBlock}>
+        <Text style={styles.header}> About HotSoup</Text>
+        <Text style={styles.text}>
+          HotSoup is an OpenSource project started by Robin Weitzman. It 
+          is an entirely volunteer-based, co-op project to build a free
+          app those in need of a free meal. {"\n"}{"\n"}
+          This current version is the first iteration of this project so there may be some errors. If you'd like 
+          to know more about this project or report any errors, contact robin@hotsoup.com.
+          We appreciate your patience and assistance as we build further versions of this app!{"\n"}
+        </Text>
+        <StatusBar style="auto" />
+      </View> 
     </View>
   );
 }
@@ -32,16 +34,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
-    alignItems: "center"
+    alignItems: "center",
+    backgroundColor: "#fcb36a"
+  },
+  textBlock: {
+    borderRadius: 25,
+    padding: 10,
+    alignItems: "center",
+    backgroundColor: "rgba(52, 52, 52, 0.8)",
+    transparency: "50%"
   },
   text: {
-    color: "#000",
-    fontSize: 14,
+    color: "#FFF",
+    fontSize: 28,
     fontWeight: "bold"
   },
   header: {
-    color: "#61dafb",
-    fontSize: 30,
+    color: "#fff",
+    fontSize: 42,
     marginBottom: 36
   }
 });
