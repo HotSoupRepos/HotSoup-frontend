5b56e1123f7530f7f0feb4b5c7b08032f1bc2200
Robin Weitzman <rl_weitzman@yahoo.com>
2022-07-22 22:11:12 -0700

    working on implementing tab and stack navigation together

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,81 @@
+import React, { FC } from "react"
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { registerRootComponent } from "expo";
 import { Home, MapList } from "@screens";
+import { View, Text, Button, StyleSheet} from "react-native";
 
-const Stack = createNativeStackNavigator();
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-type RootStackParamList = {
+export type RootStackParamList = {
   Home: undefined; // undefined because you aren't passing any params to the home screen
   MapList: undefined;
+  About: undefined;
 };
 
-type ProfileScreenNavigationProp = StackNavigationProp<
-  RootStackParamList,
-  "Home"
->;
+// type ProfileScreenNavigationProp = StackNavigationProp<
+//   RootStackParamList,
+//   "Home"
+// >;
 
-type Props = {
-  navigation: ProfileScreenNavigationProp;
+const AboutScreen = () => {
+  return <View style={styles.container}><Text>About page</Text></View>;
 };
 
-const HomeScreen = () => {
-  return <Home></Home>;
-};
+const HomeStack = createNativeStackNavigator<RootStackParamList>();
 
-const MapListScreen = () => {
-  return <MapList></MapList>;
-};
+const HomeStackScreen:FC<RootStackParamList> =() => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={Home} />
+      <HomeStack.Screen name="MapList" component={MapList} />
+    </HomeStack.Navigator>
+  );
+}
+
+const MapListStack = createNativeStackNavigator<RootStackParamList>();
+
+const MapListStackScreen:FC<RootStackParamList> = () => {
+  return (
+    <MapListStack.Navigator>
+      <MapListStack.Screen name="MapList" component={MapList} />
+      <MapListStack.Screen name="About" component={AboutScreen} />
+    </MapListStack.Navigator>
+  );
+}
+
+const AboutStack = createNativeStackNavigator<RootStackParamList>();
+
+const AboutStackScreen:FC<RootStackParamList> = () => {
+  return (
+    <AboutStack.Navigator>
+      <AboutStack.Screen name="About" component={AboutScreen} />
+    </AboutStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator<RootStackParamList>();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="MapList" component={MapListScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="MapList" component={MapListStackScreen} />
+        <Tab.Screen name="About" component={AboutStackScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default registerRootComponent(App);
