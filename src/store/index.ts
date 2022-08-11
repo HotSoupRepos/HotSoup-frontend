8dbda2c7039427225fae370980f8b41dccca50a2
Dan Ko <danko56666@gmail.com>
2022-08-10 22:05:59 -0400

    setup redux-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import soupKitchenReducer, {
   selectLocations,
@@ -8,12 +19,31 @@ import soupKitchenReducer, {
 
 export { selectLocations, getLocationsAsync, SoupKitchenModel };
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage: AsyncStorage,
+  whitelist: ["soupKitchen"],
+};
+
+const rootReducer = combineReducers({
+  soupKitchen: soupKitchenReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // had to keep this in same file to not get warnings about require cycles...very annoying :P
 export const store = configureStore({
-  reducer: {
-    soupKitchen: soupKitchenReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 type AppDispatch = typeof store.dispatch;
