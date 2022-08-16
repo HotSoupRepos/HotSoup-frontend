752a08654af19fc17a3643291844cfe4127377fe
chirani <chirani124@gmail.com>
2022-08-16 20:51:44 +0100

    changed order of variables

--- src/__tests__/LocationList.test.js
+++ src/__tests__/LocationList.test.js
@@ -3,14 +3,7 @@ import renderer from "react-test-renderer";
 import { LocationList } from "@components";
 
 describe("Testing : <LocationList />", () => {
-  const mockLocations = [
-    {
-      name: "The Great Pyramid of Giza",
-      address: "main street",
-      phone: "2020-2020",
-      lat: "29.9792",
-      long: "31.1342",
-    },
+  const mockLocation = [
     {
       name: "Leaning Tower of Pisa",
       address: "main street",
@@ -19,7 +12,14 @@ describe("Testing : <LocationList />", () => {
       long: "10.3966",
     },
   ];
-  const mockLocation = [
+  const mockLocations = [
+    {
+      name: "The Great Pyramid of Giza",
+      address: "main street",
+      phone: "2020-2020",
+      lat: "29.9792",
+      long: "31.1342",
+    },
     {
       name: "Leaning Tower of Pisa",
       address: "main street",
