e7c49dc703552a64d57be8d205866e1c8ef81c85
Jaldeep <jaldeepsenjaliya@gmail.com>
2022-08-19 00:18:51 -0400

    HOT-23 update LocationList component

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -3,40 +3,39 @@ import { View, StyleSheet, Text, Pressable, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 type props = {
-    locationName: string
-    distance: string
-}
+  locationName: string,
+};
 
-export default function ListItem ({locationName, distance} : props) {
-    
-    const handlePress = () => {
-        console.log("Info button cliked!");
-      };
+export default function ListItem({ locationName }: props) {
+  const handlePress = () => {
+    console.log("Info button cliked!");
+  };
 
   return (
     <View style={styles.listContainer}>
-        <View style={styles.locationContainer}>
-            <Text style={styles.locationText}>{locationName}</Text>
+      <View style={styles.locationContainer}>
+        <Text style={styles.locationText}>
+          {locationName}
+        </Text>
+      </View>
+      <View style={styles.distanceContainer}>
+        {/* Currently distance is hard coded beacuse there's no distance data from an API*/}
+        <Text style={styles.distanceText}>0.2 mi</Text>
+      </View>
+      <Pressable onPress={handlePress} style={styles.infoBtn}>
+        <View>
+          <Ionicons name="information-circle-outline" size={30} color="white" />
         </View>
-        <View style={styles.distanceContainer}>
-            <Text style={styles.distanceText}> {distance}</Text>
-        </View>
-        <Pressable onPress={handlePress} style={styles.infoBtn}>
-            <View>
-            <Ionicons
-                name="information-circle-outline"
-                size={30}
-                color="white"
-              />
-            </View>
-        </Pressable>
+      </Pressable>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   listContainer: {
-    marginTop: 10,
+    width: "100%",
+    height: "10%",
+    marginBottom: "0.5%",
     backgroundColor: "#232323",
     flexDirection: "row",
     justifyContent: "space-between",
@@ -44,39 +43,40 @@ const styles = StyleSheet.create({
   locationContainer: {
     backgroundColor: "#232323",
     flex: 0.75,
-    marginLeft: 12,
-    marginTop: 20,
-    marginBottom: 20,
-    height: 35,
+    marginLeft: "2%",
+    marginTop: "5%",
+    marginBottom: "5%",
+    height: "50%",
   },
   locationText: {
-    color: "#ffffff",
+    color: "#fff",
     fontSize: 18,
-    paddingTop: 5,
-    paddingBottom: 5,
-    paddingRight: 5,
+    textAlign: "left",
+    paddingTop: "1.5%",
+    paddingBottom: "1.5%",
+    paddingRight: "0.5%",
   },
   distanceContainer: {
     flex: 0.14,
     backgroundColor: "#4F4F4F",
     borderRadius: 5,
-    marginLeft: 7,
-    marginTop: 22.5,
-    marginBottom: 20,
-    height: 30,
+    marginLeft: "0.5%",
+    marginTop: "5%",
+    marginBottom: "5%",
+    height: "40%",
   },
   distanceText: {
     color: "#B2FFC3",
-    paddingLeft: 5,
-    paddingTop: 5,
-    paddingBottom: 5,
-    paddingRight: 5,
+    marginLeft: "10%",
+    marginTop: "10%",
+    marginBottom: "10%",
+    marginRight: "10%",
   },
   infoBtn: {
     flex: 0.1,
-    marginLeft: 10,
-    marginTop: 22.5,
-    marginRight: 12,
-    marginBottom: 20,
+    marginLeft: "1.5%",
+    marginTop: "5%",
+    marginRight: "0.5%",
+    marginBottom: "5%",
   },
 });
