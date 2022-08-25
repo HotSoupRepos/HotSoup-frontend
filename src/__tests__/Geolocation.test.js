7af5ac4b344977639d5919eb763870b751893d8c
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-08-25 18:32:42 -0500

    Created testing file for Geolocation but have no clue what Im doing, Added comments for current thought process

---
+++ src/__tests__/Geolocation.test.js
@@ -0,0 +1,5 @@
+//TODO - add testing for geolocation finding/tracking... some how!
+
+// -- General Thoughts --
+// Should I test if permissions is being asked since that happens first?
+// Maybe see if Permissions has been asked THEN test for geolocation?? 
